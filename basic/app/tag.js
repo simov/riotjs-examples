72960532c46065f66dd54b57a188e4b8bb115fa0
simov <simeonvelichkov@gmail.com>
2016-01-15 16:14:01 +0200

    Update tag wrapper

--- basic/app/tag.js
+++ basic/app/tag.js
@@ -1,5 +1,10 @@
 
-app.tag = function (name, options) {
+app.tag = function (name, options, inline) {
+  Object.keys(app.tags).forEach(function (tag) {
+    if (tag !== name) {
+      app.tags[tag].unmount(true)
+    }
+  })
   if (app.tags[name]) {
     if (!app.tags[name].isMounted) {
       app.tags[name] = r.mount(name, options)[0]
@@ -10,15 +15,21 @@ app.tag = function (name, options) {
     }
   }
   else {
-    $.ajax({
-      type: 'GET',
-      url: name + '.html',
-      dataType: 'text',
-      success: function (html) {
-        r.tag(name, html, app.controller[name])
-        app.tags[name] = r.mount(name, options)[0]
-        app.controller[name].cleanup()
-      }
-    })
+    if (inline) {
+      r.tag(name, window[name].innerHTML, app.controller[name])
+      app.tags[name] = r.mount(name, options)[0]
+    }
+    else {
+      $.ajax({
+        type: 'GET',
+        url: name + '.html',
+        dataType: 'text',
+        success: function (html) {
+          r.tag(name, html, app.controller[name])
+          app.tags[name] = r.mount(name, options)[0]
+          app.controller[name].cleanup()
+        }
+      })
+    }
   }
 }
